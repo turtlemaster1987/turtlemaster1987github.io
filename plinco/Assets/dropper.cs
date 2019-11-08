686beae699da8fe66897758dd72dd6ea63d73f75
turtlemaster1987 <56265227+turtlemaster1987@users.noreply.github.com>
2019-11-08 10:10:29 -0600

    games
    games

---
+++ plinco/Assets/dropper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropperScript : MonoBehaviour
+{
+    public float speed;
+    public GameObject ball;
+
+    private int score;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDir = Vector3.zero;
+        moveDir.x = Input.GetAxis("Horizontal");
+        transform.position += moveDir * speed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(ball, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
